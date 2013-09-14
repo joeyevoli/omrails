368d550379173607392b81a0175fabf6aeafb938
Joe Yevoli <joe.yevoli@gmail.com>
2013-09-13 21:12:15 -0400

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # configuring Amazon S3 for paperclip file uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
